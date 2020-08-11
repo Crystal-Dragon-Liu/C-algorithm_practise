abf6f0f317b11b2e478a6fbb68e71197919daeb2
Crystal-Dragon-Liu <LybCrystal@163.com>
2020-08-11 20:50:24 +0800

    多线程

---
+++ C_algorithm/C_algorithm/MultThreadPractise/MultThreadPractise/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  MultThreadPractise
+//
+//  Created by  云子谣 on 2020/8/11.
+//  Copyright © 2020 LybCrystal@163.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
+
